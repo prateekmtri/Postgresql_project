5b42d6d65008f40e637c49a8f7557a01bcc8e4eb
prateekmtri <prateek1tri2@gmail.com>
2026-01-31 15:41:10 +0530

    Step 5: Insert records into employees (default, FK in action)

---
+++ sql/05_insert_employees.sql
@@ -0,0 +1,7 @@
+\c company_db;
+
+INSERT INTO employees (emp_id, name, email, salary, dept_id)
+VALUES
+(1, 'Ram', 'ram@gmail.com', 20000, 10),
+(2, 'Shyam', 'shyam@gmail.com', 25000, 20),
+(3, 'Mohan', 'mohan@gmail.com', 30000, 30);
